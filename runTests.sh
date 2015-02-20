9e375aa3cbee131baeee618975deea1572672099
unknown <poojar@INpoojar.corporate.thoughtworks.com>
2015-02-20 09:29:22 +0530

    TextFile is renamed to WC

--- runTests.sh
+++
@@ -1,8 +0,0 @@
-rm *.class
-echo "compiling ......"
-echo "____________________________________________________"
-javac -cp junit-4.10.jar TextFileTest.java TextFile.java
-echo "running Tests ......"
-echo "____________________________________________________"
-java -cp ".;junit-4.10.jar" org.junit.runner.JUnitCore TextFileTest
-
